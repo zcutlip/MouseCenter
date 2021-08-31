c9c1c387c9abb3cc42ead179de79e7523a197601
Zachary Cutlip <uid000@gmail.com>
2021-08-31 11:24:39 -0700

    initial commit

---
+++ MousCenter.spoon/init.lua
@@ -0,0 +1,38 @@
+local obj = {}
+obj.__index = obj
+
+-- Metadata
+obj.name = "MouseCenter"
+obj.version = "1.0"
+obj.author = "Zachary Cutlip"
+obj.homepage = ""
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+obj.circle = nil
+obj.timer = nil
+obj.hotkey = nil
+obj.mcspoon = hs.loadSpoon("MouseCircle")
+
+function obj:bindHotkeys(mapping)
+    if (self.hotkey) then
+        self.hotkey:delete()
+    end
+    local centerMods = mapping["center"][1]
+    local centerKey = mapping["center"][2]
+    self.hotkey = hs.hotkey.bind(centerMods, centerKey, function() self:center() end)
+
+    return self
+end
+
+
+function obj:center()
+    local ps = hs.screen.primaryScreen()
+    local ff = ps:fullFrame()
+    local center = ps:absoluteToLocal(ff).center
+    hs.mouse.setRelativePosition(center, ps)
+    if self.mcspoon then
+        self.mcspoon:show()
+    end
+end
+
+return obj
